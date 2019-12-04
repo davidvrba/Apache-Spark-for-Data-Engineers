f1136a596fdcde801faca76bee2aa15ed7989c99
davidvrba <vrba.dave@gmail.com>
2019-12-04 17:29:43 +0100

    install updated

--- conf/install.sh
+++ conf/install.sh
@@ -3,7 +3,9 @@ apt-get  update
 apt-get --assume-yes install openjdk-8-jdk
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
 apt-get --assume-yes install scala
-sudo apt install maven
+apt-get --assume-yes install maven
+apt-get --assume-yes install jq
+apt-get --assume-yes install curl
 
 wget /home/student https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
 tar xvf spark-2.4.4-bin-hadoop2.7.tgz
@@ -25,12 +27,17 @@ sudo -u postgres bash -c "psql -c \"ALTER USER postgres PASSWORD 'postgres'; \" 
 
 source ~/.bashrc
 
-cd ..
-cd tests
-
 git clone https://github.com/apache/zeppelin.git
 
-cd zeppelin
+cd /home/student/zeppelin
+
 mvn clean package -DskipTests
 
+cp /home/student/zeppelin/conf/zeppelin-env.sh.template /home/student/zeppelin/conf/zeppelin-env.sh
+
+echo 'export ZEPPELIN_NOTEBOOK_DIR=/home/student/Apache-Spark-for-Data-Engineers/scala' >> /home/student/zeppelin/conf/zeppelin-env.sh
+
 ./bin/zeppelin-daemon.sh start
+
+#should return "2.4.4\n"
+curl -X POST http://localhost:8080/api/notebook/run/2EWCBR9G2/paragraph_1575474190259_-1578016672 | jq '.body.msg'[0]'.data'
