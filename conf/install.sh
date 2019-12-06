1918d7a7e7ffcf64e883cea4976aea24052a8516
David Vrba <david.vrba@vrbad.local>
2019-12-06 17:30:14 +0100

    install updated

--- conf/install.sh
+++ conf/install.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-#apt-get  update
-#apt-get --assume-yes install openjdk-8-jdk
-#echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
-#apt-get --assume-yes install scala
+apt-get  update
+apt-get --assume-yes install openjdk-8-jdk
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
+apt-get --assume-yes install scala
 #apt-get --assume-yes install maven
-#apt-get --assume-yes install jq
-#apt-get --assume-yes install curl
+apt-get --assume-yes install jq
+apt-get --assume-yes install curl
 
-#wget /home/student https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
-#tar xvf spark-2.4.4-bin-hadoop2.7.tgz
+wget /home/student https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+tar xvf spark-2.4.4-bin-hadoop2.7.tgz
 
-#cp /home/student/Apache-Spark-for-Data-Engineers/conf/hive-site.xml /home/student/spark-2.4.4-bin-hadoop2.7/conf/
+cp /home/student/Apache-Spark-for-Data-Engineers/conf/hive-site.xml /home/student/spark-2.4.4-bin-hadoop2.7/conf/
 
-#echo 'export SPARK_HOME=/home/student/spark-2.4.4-bin-hadoop2.7' >> ~/.bashrc
+echo 'export SPARK_HOME=/home/student/spark-2.4.4-bin-hadoop2.7' >> ~/.bashrc
 
-#SPARK_HOME=/home/student/spark-2.4.4-bin-hadoop2.7
+SPARK_HOME=/home/student/spark-2.4.4-bin-hadoop2.7
 
-#wget -P /home/student/spark-2.4.4-bin-hadoop2.7/jars https://jdbc.postgresql.org/download/postgresql-9.4.1212.jar
+wget -P /home/student/spark-2.4.4-bin-hadoop2.7/jars https://jdbc.postgresql.org/download/postgresql-9.4.1212.jar
 
-#apt --assume-yes install postgresql postgresql-contrib
-#sudo -u postgres bash -c "psql -c \"CREATE DATABASE metastore; \""
-#sudo -u postgres bash -c "psql -c \"CREATE USER hiveuser with encrypted password 'hiveuser'; \""
-#sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES on database metastore to hiveuser; \""
+apt --assume-yes install postgresql postgresql-contrib
+sudo -u postgres bash -c "psql -c \"CREATE DATABASE metastore; \""
+sudo -u postgres bash -c "psql -c \"CREATE USER hiveuser with encrypted password 'hiveuser'; \""
+sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES on database metastore to hiveuser; \""
 
-#sudo -u postgres bash -c "psql -c \"ALTER USER postgres PASSWORD 'postgres'; \" "
+sudo -u postgres bash -c "psql -c \"ALTER USER postgres PASSWORD 'postgres'; \" "
 
-#source ~/.bashrc
+source ~/.bashrc
 
 #git clone https://github.com/apache/zeppelin.git
 #cd /home/student/zeppelin
 #mvn clean package -DskipTests
 
-#wget /home/student http://apache.miloslavbrada.cz/zeppelin/zeppelin-0.8.2/zeppelin-0.8.2-bin-all.tgz
-#tar xvf zeppelin-0.8.2-bin-all.tgz
+wget /home/student http://apache.miloslavbrada.cz/zeppelin/zeppelin-0.8.2/zeppelin-0.8.2-bin-all.tgz
+tar xvf zeppelin-0.8.2-bin-all.tgz
 
 cp /home/student/zeppelin-0.8.2-bin-all/conf/zeppelin-env.sh.template /home/student/zeppelin-0.8.2-bin-all/conf/zeppelin-env.sh
 
