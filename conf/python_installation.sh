3381b52d28c01e930c65b65ab382fddd42e72f8f
vrbad <david.vrba@emplifi.io>
2024-10-06 15:17:14 +0200

    update for Spark 3.5.3

--- conf/python_installation.sh
+++ conf/python_installation.sh
@@ -3,27 +3,34 @@
 student=$1
 
 apt-get  update
-apt install python3.7
 apt --assume-yes install python3-pip
 apt-get --assume-yes install openjdk-8-jdk
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
 apt-get --assume-yes install scala
-pip3 install py4j
-pip3 install delta-spark
-pip3 install jupyter
+pip3 install py4j  --break-system-packages
+pip3 install delta-spark==3.2.1 --break-system-packages
+apt --assume-yes remove python3-jsonschema
+pip3 install jsonschema --break-system-packages
+pip3 install jupyter --break-system-packages
 
-wget /home/$student https://archive.apache.org/dist/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
-tar xvf spark-3.3.2-bin-hadoop3.tgz
+wget /home/$student https://archive.apache.org/dist/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
+tar xvf spark-3.5.3-bin-hadoop3.tgz
 
-cp /home/$student/Apache-Spark-for-Data-Engineers/conf/hive-site.xml /home/$student/spark-3.3.2-bin-hadoop3/conf/
+cp /home/$student/Apache-Spark-for-Data-Engineers/conf/hive-site.xml /home/$student/spark-3.5.3-bin-hadoop3/conf/
 
-SPARK_HOME=/home/$student/spark-3.3.2-bin-hadoop3
+SPARK_HOME=/home/$student/spark-3.5.3-bin-hadoop3
 
+echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
 echo 'export SPARK_HOME='$SPARK_HOME >> ~/.bashrc
 echo 'export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 
-wget -P /home/$student/spark-3.3.2-bin-hadoop3/jars https://jdbc.postgresql.org/download/postgresql-42.4.0.jar
+wget -P /home/$student/spark-3.5.3-bin-hadoop3/jars https://jdbc.postgresql.org/download/postgresql-42.4.0.jar
+
+mkdir /home/ubuntu/.jupyter
+cp /home/$student/Apache-Spark-for-Data-Engineers/conf/jupyter_notebook_config.py /home/$student/.jupyter/
+
+chown -R ubuntu:ubuntu /home/ubuntu/.jupyter
 
 source ~/.bashrc
 
